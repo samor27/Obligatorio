8c38425de0a03847a805abff8a2bb1518b1dbd83
Valevilla2 <villadacosta03@gmail.com>
2025-05-16 09:30:31 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -1,6 +1,14 @@
 class Sistema:
-  def__init__(self,clientes,piezas,maquinas,pedido):
+  def__init__(self,max_clientes,clientes,piezas,maquinas,pedido):
     self.clientes=[]
     self.piezas=[]
     self.maquinas=[]
     self.pedidos=[]
+    self.__max_clientes=0
+  @Property
+  def max_clientes(self):
+    return self.__max_clientes
+  @max_clientes.setter
+  def max_clientes(self,num):
+    self.__max_clientes=num
+    
