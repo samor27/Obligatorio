d1bdfc7522afd9417c1c13bef6e7f33d67a38270
samor27 <samor1@correo.um.edu.uy>
2025-05-16 14:16:25 -0300

    .

--- pedido.py
+++ pedido.py
@@ -17,17 +17,17 @@ class Pedido:
         self.__precio = pre
 
 class Sistema:
-  def__init__(self,max_clientes,clientes,piezas,maquinas,pedido):
-    self.clientes=[]
-    self.piezas=[]
-    self.maquinas=[]
-    self.pedidos=[]
-    self.__max_clientes=0
-  @Property
+  def __init__(self,max_clientes,clientes,piezas,maquinas,pedido):
+      self.clientes=[]
+      self.piezas=[]
+      self.maquinas=[]
+      self.pedidos=[]
+      self.__max_clientes=0
+  @property
   def max_clientes(self):
     return self.__max_clientes
   @max_clientes.setter
   def max_clientes(self,num):
     self.__max_clientes=num
-  def aumentar_clientes
+  def aumentar_clientes():
     max_clientes +=1
