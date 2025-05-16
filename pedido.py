bac0365e291f0c0c4119d251bc48cd35cef03d05
samor27 <samor1@correo.um.edu.uy>
2025-05-16 14:57:47 -0300

    .

--- pedido.py
+++ pedido.py
@@ -15,19 +15,3 @@ class Pedido:
     @precio.setter
     def precio (self, pre):
         self.__precio = pre
-
-class Sistema:
-  def __init__(self,max_clientes,clientes,piezas,maquinas,pedido):
-      self.clientes=[]
-      self.piezas=[]
-      self.maquinas=[]
-      self.pedidos=[]
-      self.__max_clientes=0
-  @property
-  def max_clientes(self):
-    return self.__max_clientes
-  @max_clientes.setter
-  def max_clientes(self,num):
-    self.__max_clientes=num
-  def aumentar_clientes():
-    max_clientes +=1
