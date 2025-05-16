ca216528a2c391bd5a94dc9ce82d8eb6c59af5b3
samor27 <samor1@correo.um.edu.uy>
2025-05-16 10:03:23 -0300

    .

---
+++ pedido.py
@@ -0,0 +1,17 @@
+
+import datetime
+class Pedido:
+    def __init__(self, cliente, maquina, fecha_entregado, estado):
+        self.cliente = cliente
+        self.maquina = maquina
+        self.fecha_recibido = datetime.date
+        self.fecha_entregado = fecha_entregado
+        self.estado = estado #pendiente o entregado
+    
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @precio.setter
+    def precio (self, precio):
+        self.__precio = precio
