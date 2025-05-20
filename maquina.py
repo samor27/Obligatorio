5d56e03ba6d827c5b688b75d2ff1bd9c20b14319
samor27 <samor1@correo.um.edu.uy>
2025-05-20 08:31:17 -0300

    .

---
+++ maquina.py
@@ -0,0 +1,2 @@
+class Maquina:
+    pass
--- pedido.py
+++ pedido.py
@@ -15,3 +15,15 @@ class Pedido:
     @precio.setter
     def precio (self, pre):
         self.__precio = pre
+
+    def select_cliente(self):
+        print ("Clientes: ") #Lista de clientes
+        #for i in range (clientes):
+            #print clientes[i]
+        self.cliente = int(input("Introduzca el número de cliente"))
+
+    def select_maquina(self):
+        print("Maquinas: ") #Lista de Maquinas
+        #for i in range (maquina):
+            #print maquina[i]
+        self.maquina = int(input("Introduzca el número de la maquina que quiera pedir: "))
