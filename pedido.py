4a2c670d612277e3cba5bfc9c2daacb472086f33
samor27 <samor1@correo.um.edu.uy>
2025-05-20 10:38:37 -0300

    .

--- pedido.py
+++ pedido.py
@@ -1,10 +1,11 @@
 
 import datetime
+#import costo_de_produccion from Maquina
 class Pedido:
     def __init__(self, cliente, maquina, fecha_entregado, estado):
         self.cliente = cliente
         self.maquina = maquina
-        self.fecha_recibido = datetime.date
+        self.fecha_recibido = (datetime.date, datetime.time)
         self.fecha_entregado = fecha_entregado
         self.estado = estado #pendiente o entregado
     
@@ -13,10 +14,14 @@ class Pedido:
         return self.__precio
     
     @precio.setter
-    def precio (self, pre):
-        self.__precio = pre
+    def precio (self, costo_produccion):
+        pre = (costo_produccion + (costo_produccion*0.5))
+        # if self.cliente == Empresa:
+        #     self.__precio = pre*0,8
+        # else:
+        #     self.__precio = pre
 
-    def select_cliente(self):
+    def select_cliente():
         print ("Clientes: ") #Lista de clientes
         #for i in range (clientes):
             #print (i, clientes[i])
@@ -25,14 +30,15 @@ class Pedido:
             num = int(input("Introduzca el número de cliente"))
             # for i in range (clientes):
             #     if num == i:
-            #         self.cliente = clientes[i]
+            #         cliente = clientes[i]
             #         cond = True
             #     else:
             #         print("El numero ingresado no corresponde con ningun cliente./
             #          Pruebe de nuevo")
+            #return cliente
 
 
-    def select_maquina(self):
+    def select_maquina():
         print("Maquinas: ") #Lista de Maquinas
         #for i in range (maquina):
             #print (i, maquina[i])
@@ -41,7 +47,10 @@ class Pedido:
             num = int(input("Introduzca el número de la maquina que quiera pedir: "))
             # for i in range (maquina):
             #     if num == i:
-            #         self.maquina = maquina[i]
+            #         maquina = maquina[i]
+            #         cond = True
             #     else:
-            #         print("El numero ingresado no corresponde con ningun cliente./
+            #         print("El numero ingresado no corresponde con ninguna Maquina./
             #          Pruebe de nuevo")
+        #return maquina
+    
