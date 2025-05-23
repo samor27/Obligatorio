2008b223da20ec58f3e99afaa35c13b40ff67ddd
juana6 <jasantoslopez4@gmail.com>
2025-05-23 08:48:01 -0300

    Update pieza.py

--- pieza.py
+++ pieza.py
@@ -1,2 +1,7 @@
 class Pieza:
-    pass
+    def __init__(self,codigo,descripcion,costo,tamaño_lote,cantidad_disponible):
+        self.codigo = codigo
+        self.descripcion = descripcion
+        self.costo = costo
+        self.tamaño_lote = tamaño_lote
+        self.cantidad_disponible = cantidad_disponible
