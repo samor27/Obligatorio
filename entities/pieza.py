70ca8feeaa2b91fcdb18515e362cb5aa0253d191
juana6 <jasantoslopez4@gmail.com>
2025-05-27 08:45:50 -0300

    Update pieza.py

--- entities/pieza.py
+++ entities/pieza.py
@@ -1,10 +1,55 @@
 class Pieza:
     def __init__(self,codigo,descripcion,costo,tamaño_lote,cantidad_disponible):
-        self.codigo = codigo
-        self.descripcion = descripcion
-        self.costo = costo
+        self.__codigo = codigo
+        self.__descripcion = descripcion
+        self.__costo = costo
+        self.__tamaño_lote = tamaño_lote
+        self.__cantidad_disponible = cantidad_disponible
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @codigo.setter
+    def codigo(self,codigo):
+        self.__codigo = codigo
+
+    @property
+    def descripcion(self):
+        return self.__descripcion
+    
+    @descripcion.setter
+    def descripcion(self,descripcion):
+        self.__descripcion = descripcion
+
+    @property
+    def costo(self):
+        return self.__costo
+    
+    @costo.setter
+    def costo(self,costo):
+        self.__costo = costo
+
+    @property
+    def tamaño_lote(self):
+        return self.__tamaño_lote
+    
+    @tamaño_lote.setter
+    def tamaño_lote(self,tamaño_lote):
         self.tamaño_lote = tamaño_lote
+
+    @property
+    def cantidad_disponible(self):
+        return self.__cantidad_disponible
+    
+    @cantidad_disponible.setter
+    def cantidad_disponible(self,cantidad_disponible):
         self.cantidad_disponible = cantidad_disponible
+
+
+        
+
+    
     
     def agregar_stock(self,cantidad):
         self.cantidad_disponible += cantidad
@@ -14,6 +59,9 @@ class Pieza:
             raise ValueError("No hay suficiente stock disponible")
         self.cantidad_disponible -= cantidad   
 
+   
+    
+   
 
    
 
