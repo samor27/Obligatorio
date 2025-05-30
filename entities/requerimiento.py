b9172ec041f4b2e0bf78a5f66146f08f2afbbf3e
juana6 <jasantoslopez4@gmail.com>
2025-05-30 09:21:38 -0300

    Update requerimiento.py

--- entities/requerimiento.py
+++ entities/requerimiento.py
@@ -1,7 +1,16 @@
-from maquina import Maquina
-from entities.pieza import Pieza
 class Requerimiento:
-    def __init__(self, maquina: Maquina, pieza, cantidad):
-        self.maquina = maquina
-        self.pieza = pieza
-        self.cantidad = cantidad
+    def __init__(self,pieza,cantidad):
+        self.__pieza = pieza
+        self.__cantidad = cantidad
+    @property
+    def pieza(self):
+        return self.__pieza
+    @pieza.setter
+    def pieza(self,pieza):
+        self.__pieza = pieza
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    @cantidad.setter
+    def cantidad(self,cantidad):
+        self.__cantidad = cantidad
