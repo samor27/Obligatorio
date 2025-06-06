c121d2b6c52040e5b4aae6e5d7978a07ae0e48cb
Valevilla2 <villadacosta03@gmail.com>
2025-06-06 08:23:57 -0300

    Update reposicion.py

--- entities/reposicion.py
+++ entities/reposicion.py
@@ -1,2 +1,30 @@
-class Reposicion:
-    pass
+import datetime
+class Reposición:
+    def __init__ (self, pieza, cantidad_lotes):
+        self.__pieza= pieza
+        self.__cantidad_lotes=(cantidad_lotes)
+        self.__fecha=(datetime.date,datetime.time)
+    
+    @property
+    def pieza(self):
+        return self.__pieza
+    
+    @pieza.setter
+    def pieza(self,pieza):
+        self.__pieza = pieza
+
+    @property
+    def cantidad_lotes(self):
+        return self.__cantidad_lotes
+    
+    @cantidad_lotes.setter
+    def cantidad_lotes(self,cl):
+        self.__cantidad_lotes = cl
+
+    @property
+    def fecha(self):
+        return self.__fecha
+    
+    @fecha.setter
+    def fecha(self,co):
+        self.__fecha = co
